830f25cd8c53b40ae704c251e048c1ed86beaddc
Jesse Goodall <jessegoodall@gmail.com>
2015-01-03 14:00:47 -0500

    add rating to feed

--- js/directives.js
+++ js/directives.js
@@ -1,1 +1,43 @@
-angular.module('app.directives', []);
+angular.module('app.directives', [])
+  // TODO this can be replaced by the rating implementation in angular-bootstrap
+  .directive('rating', function () {
+    return {
+      restrict: 'E',
+      template: '<ul class="rating">' +
+                  '<li ng-repeat="star in stars" ng-class="star" ng-click="toggle($index)">' +
+                    '\u2605' +
+                  '</li>' +
+                '</ul>',
+      scope: {
+        ratingValue: '=',
+        max: '=',
+        readonly: '@',
+        onRatingSelected: '&'
+      },
+      link: function (scope, elem, attrs) {
+
+        var updateStars = function() {
+          scope.stars = [];
+          for (var  i = 0; i < scope.max; i++) {
+            scope.stars.push({filled: i < scope.ratingValue});
+          }
+        };
+
+        scope.toggle = function(index) {
+          if (scope.readonly && scope.readonly === 'true') {
+            return;
+          }
+          scope.ratingValue = index + 1;
+          scope.onRatingSelected({rating: index + 1});
+        };
+
+        scope.$watch('ratingValue', function(oldVal, newVal) {
+          if (newVal != oldVal || oldVal == 0) {
+            updateStars();
+          }
+        });
+
+        updateStars();
+      }
+    }
+  });
